727dc2b9e0dffde2ae7e06b09dcce3952c0ce3dd
CeruleanHokori <razani.ziad0@gmaiL.com>
2022-10-30 12:16:32 +0100

    First function for blob detection added, read comment for more info

--- morpho.py
+++ morpho.py
@@ -2,30 +2,39 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-eight = cv2.imread("./images/8.png",cv2.IMREAD_GRAYSCALE)
-(thresh, BWeight) = cv2.threshold(eight, 127, 255, cv2.THRESH_BINARY)
-threshold = 0
-eight[eight>threshold]=1
-kernel = np.ones((8,8),np.uint8)
-eroded_eight = cv2.erode(eight,kernel,iterations=1)
-# Contours using findContours
-# contours, hierarchy = cv2.findContours(eroded_eight,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-copy = eroded_eight.copy()
-# cv2.drawContours(copy,contours,-1,(255,0,0), 1)
-mask = np.zeros((copy.shape[0]+2,copy.shape[1]+2),np.uint8)
-_,img,_,_ = cv2.floodFill(copy,mask,(0,0),1)
-dilated_img = cv2.dilate(img,kernel=np.ones((6,6),np.uint8))
-plt.subplot(1,3,1)
-plt.imshow(BWeight,cmap='gray')
-plt.subplot(1,3,2)
-plt.imshow(eroded_eight,cmap='gray')
-plt.subplot(1,3,3)
-# plt.imshow(copy)
-plt.imshow(dilated_img,cmap='gray')
-plt.show()
-s = []
-for elt in eight:
-    for pixel in elt:
-        if pixel not in s:
-            s.append(pixel)
-print(s)
+#! Remark: Erosion depends on the thickness of the digit
+#! If the digit is too thin, erosion will delete its borders
+def number_of_blobs(filename, plot_result=False):
+    #Loading and Thresholding
+    eight = cv2.imread(filename,cv2.IMREAD_GRAYSCALE)
+    (thresh, BWeight) = cv2.threshold(eight, 127, 255, cv2.THRESH_BINARY)
+    threshold = 0
+    eight[eight>threshold]=1
+    #Erosion
+    kernel = np.ones((2,2),np.uint8)
+    eroded_eight = cv2.erode(eight,kernel,iterations=1)
+    #Flood filling to find holes
+    flooded = eroded_eight.copy()
+    mask = np.zeros((flooded.shape[0]+2,flooded.shape[1]+2),np.uint8)
+    _,flooded,_,_ = cv2.floodFill(flooded,mask,(0,0),1)
+    #Dilation
+    dilated_img = cv2.dilate(flooded,kernel=np.ones((6,6),np.uint8))
+    #Finding blobs by contouring them
+    contour_blobs,_ = cv2.findContours(dilated_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    blank = np.zeros(eight.shape,np.uint8)
+    if plot_result:
+        cv2.drawContours(blank,contour_blobs,-1,(255,255,255),3)
+        plt.subplot(1,3,1)
+        
+        plt.imshow(BWeight,cmap='gray')
+        plt.xlabel('Original')
+        plt.subplot(1,3,2)
+        plt.imshow(eroded_eight,cmap='gray')
+        plt.xlabel('Eroded')
+        plt.subplot(1,3,3)
+        plt.imshow(blank,cmap='gray')
+        plt.xlabel('Contouring blobs')
+        #plt.imshow(dilated_img,cmap='gray')
+        plt.show()
+    return len(contour_blobs) -1 #We substract 1 because findContours also detects the background which is white after floodFill
+print(number_of_blobs('./images/thick_zero.png'))
